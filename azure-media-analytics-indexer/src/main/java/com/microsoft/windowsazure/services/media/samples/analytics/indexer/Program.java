fa45bacbb30a40c0e58953d175a524e36ea2fa6e
Mariano Converti <mariano.converti@southworks.net>
2016-09-02 12:39:05 -0300

    minor bug fixes in the Media Services analytics sample

--- azure-media-analytics-indexer/src/main/java/com/microsoft/windowsazure/services/media/samples/analytics/indexer/Program.java
+++ azure-media-analytics-indexer/src/main/java/com/microsoft/windowsazure/services/media/samples/analytics/indexer/Program.java
@@ -9,6 +9,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.NoSuchAlgorithmException;
 import java.util.EnumSet;
 
@@ -81,7 +82,8 @@ public final class Program {
             System.out.println("Uploaded Asset Id: " + sourceAsset.getId());
 
             // Create indexing task configuration based on parameters
-            String indexerTaskPresetTemplate = new String(Files.readAllBytes(Paths.get(Program.class.getClassLoader().getResource("").getPath() + indexerTaskPresetTemplateFileName)));
+            String indexerTaskPresetTemplate = new String(Files.readAllBytes(
+                    Paths.get(new URL(Program.class.getClassLoader().getResource(""), indexerTaskPresetTemplateFileName).toURI())));
             String taskConfiguration = String.format(indexerTaskPresetTemplate, title, description, language, captionFormats, generateAIB, generateKeywords);
             
             // Run indexing job to generate output asset
@@ -90,7 +92,10 @@ public final class Program {
 
             // Download output asset files
             downloadAssetFiles(outputAsset, destinationPath);
-            
+
+            // Done
+            System.out.println("Sample completed!");
+
         } catch (ServiceException se) {
             System.out.println("ServiceException encountered.");
             System.out.println(se.toString());
@@ -145,7 +150,7 @@ public final class Program {
         return asset;
     }
 
-    // Create a Job that contains a Task to transform the Asset
+    // Create a Job that contains a Task to process the Asset
     private static AssetInfo runIndexingJob(AssetInfo asset, String taskConfiguration)
             throws ServiceException, InterruptedException {
         // Retrieve the list of Media Processors that match the name
@@ -183,20 +188,20 @@ public final class Program {
         // Check to see if the Job has completed
         JobState result = checkJobStatus(jobId);
         // Done with the Job
-        
+
         if (result != JobState.Finished) {
-        	System.out.println("The job has finished with a wrong status: " + result.toString());
-        	throw new RuntimeException();
+            System.out.println("The job has finished with a wrong status: " + result.toString());
+            throw new RuntimeException();
         }
 
         System.out.println("Job Finished!");
-        
+
         // Get the output Asset
         ListResult<AssetInfo> outputAssets = mediaService.list(Asset.list(job.getOutputAssetsLink()));
         AssetInfo outputAsset = outputAssets.get(0); 
-        
+
         System.out.println("Output asset: " + outputAsset.getName());
-        
+
         return outputAsset;
     }
 
@@ -206,7 +211,7 @@ public final class Program {
         while (!done) {
             // Sleep for 5 seconds
             Thread.sleep(5000);
-            
+
             // Query the updated Job state
             jobState = mediaService.get(Job.get(jobId)).getState();
             System.out.println("Job state: " + jobState);
@@ -215,7 +220,7 @@ public final class Program {
                 done = true;
             }
         }
-        
+
         return jobState;
     }
     
@@ -233,17 +238,17 @@ public final class Program {
 
         // List all the Asset Files
         ListResult<AssetFileInfo> assetFiles = mediaService.list(AssetFile.list(asset.getAssetFilesLink()));
-        
+
         for (AssetFileInfo file : assetFiles) {
             System.out.print("Downloading " + file.getName() + " output file...");
 
-            URL downloadUrl = new URL(sasLocator.getBaseUri() + file.getName() + sasLocator.getContentAccessToken());
+            URL downloadUrl = new URL(sasLocator.getBaseUri() + "/" + file.getName() + sasLocator.getContentAccessToken());
             HttpURLConnection httpConn = (HttpURLConnection) downloadUrl.openConnection();
             InputStream inputStream = httpConn.getInputStream();
-            Files.copy(inputStream, Paths.get(destinationPath, file.getName()));
+            Files.copy(inputStream, Paths.get(destinationPath, file.getName()), StandardCopyOption.REPLACE_EXISTING);
             inputStream.close();
-        	
-        	System.out.println("Done!");
+
+            System.out.println("Done!");
         }
 
         // Clean up Locator and Access Policy
